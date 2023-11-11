5fae8add136f49629824de61569f2dcd10e940c2
Chase Callahan <ctcallahan2@crimson.ua.edu>
2023-11-11 09:15:55 -0600

    Able to read through all store data csvs and combine to a single one

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+from glob import glob
+import os.path as path
+import pandas as pd
+
+# GLOBAL CONSTANTS
+AGGREGATED_STORE_FILE = "store-data-aggregate.csv"
 
 def main():
-    print("hello world")    
+    print("hello world")
+
+
+def aggregate_seperated_store_files():
+    # Get a reference to all csv files in the store-data
+    # folder that have a .csv file extension (stores them)
+    # in a list
+    store_csvs = glob("store-data/*.csv")
+
+    # Takes each file reference and converts them to a data
+    # frame. Final result is a list of data frames
+    seperated_store_dfs = list(map(csv_to_df, store_csvs))
+
+    # Concats each individual df into a single large one
+    store_df = pd.concat(seperated_store_dfs)
+
+    # Save to file so that this computation does not have
+    # to run every time
+    store_df.to_csv(AGGREGATED_STORE_FILE)
+
+    return store_df
+
+# Assumes , as delimiter by default
+def csv_to_df(file_name, delimiter=","):
+    return pd.read_csv(file_name, sep=delimiter)
 
 main()
